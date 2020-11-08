208a704e3ca8a6603f084d7ec23b04a5743cf27d
hilbug <65197724+hilbug@users.noreply.github.com>
2020-11-08 10:17:22 -0500

    add mongoose to server.js

--- server.js
+++ server.js
@@ -13,6 +13,17 @@ if (process.env.NODE_ENV === "production") {
 
 // Define API routes here
 
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
