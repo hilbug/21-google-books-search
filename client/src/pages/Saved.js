14a1cf204c1da85b7c39a658fcc6c9d3ed344d5d
hilbug <65197724+hilbug@users.noreply.github.com>
2020-11-08 23:53:59 -0500

    add pages Search and Saved

---
+++ client/src/pages/Saved.js
@@ -0,0 +1,14 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+const Saved = () => {
+    return (
+        <Container>
+            <Row>
+                <p>Something for saved here!</p>
+            </Row>
+        </Container>
+    )
+}
+
+export default Saved;
---
+++ client/src/pages/Search.js
@@ -0,0 +1,14 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+const Search = () => {
+    return (
+        <Container>
+            <Row>
+                <p>Something for search here!</p>
+            </Row>
+        </Container>
+    )
+}
+
+export default Search;
