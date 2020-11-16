5afa86ef2b295e9681b7bffe928ac1576ab3a2a9
hilbug <65197724+hilbug@users.noreply.github.com>
2020-11-16 00:59:00 -0500

    code clean-up

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -6,24 +6,24 @@ export default {
         return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}`);
     },
 
-    // From week activity
-
     // Saves a book to MongoDB
     saveBook: function (bookData) {
         return axios.post("/api/books", bookData);
     },
 
-    // TO BE BUILT
     // Gets all books saved to MongoDB
     getBooks: function () {
         return axios.get("/api/books");
     },
-    // Gets the book with the given id from MongoDB
-    getBook: function (id) {
-        return axios.get("/api/books/" + id);
-    },
+
     // Deletes the book with the given id from MongoDB
     deleteBook: function (id) {
         return axios.delete("/api/books/" + id);
+    },
+
+    // Not used
+    // Gets the book with the given id from MongoDB
+    getBook: function (id) {
+        return axios.get("/api/books/" + id);
     }
 };
